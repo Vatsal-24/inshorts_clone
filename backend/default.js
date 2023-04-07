f91b01d3daafd3f3e3e84415c04d85680c9e9871
Vatsal-24 <vatsaldoshi11@gmail.com>
2023-04-07 17:39:13 +0530

    New api used and infinite scroll done

--- backend/default.js
+++ backend/default.js
@@ -1,11 +1,12 @@
 const data = require("./constants/constants");
 const News = require("./models/newsModel");
 const axios = require("axios");
+const cors = require("cors");
 
 exports.uploadDataByDefault = async () => {
   try {
     const res = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=in&apiKey=cbc0ace88d5647bbab2daa7b7b4c042e&pageSize=100&category=general`
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.NEWS_API_KEY}&pageSize=100&category=general`
     );
     await News.insertMany(res.data.articles);
     // await News.deleteMany({});
